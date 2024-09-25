ee170d4d93e9698e0de931fc66a3e6f32ade7c2d
Vasys <vasiavasia10000@gmail.com>
2024-09-25 14:16:49 +0300

    final fix

--- src/handlers.py
+++ src/handlers.py
@@ -145,7 +145,7 @@ def register_handlers(bot):
             user_data[chat_id][processing_message_id]['audio_format'] = quality
             available_qualities = user_data[chat_id][processing_message_id]['file_info']['qualities']
             bot.edit_message_text(utils.get_string('select_quality', user_data[chat_id]['language']), chat_id, processing_message_id, reply_markup=utils.quality_keyboard(available_qualities, chat_id, processing_message_id, selected_video=user_data[chat_id][processing_message_id].get('video_format'), selected_audio=quality))
-        elif call.data == "back_to_main_":
+        elif call.data.startswith("back_to_main_"):
             processing_message_id = call.data.split("_")[-1]
             available_qualities = user_data[chat_id][processing_message_id]['file_info']['qualities']
             bot.edit_message_text(utils.get_string('select_quality', user_data[chat_id]['language']), chat_id, call.message.message_id, reply_markup=utils.quality_keyboard(available_qualities, chat_id, selected_video=user_data[chat_id]['video_format'], selected_audio=user_data[chat_id]['audio_format']))
