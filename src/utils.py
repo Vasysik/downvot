e0b771cd9271277c16b69a6965429400b83bd39b
Amadeus <vasiavasia10000@gmail.com>
2025-10-19 16:24:46 +0300

    url len fix

--- src/utils.py
+++ src/utils.py
@@ -128,7 +128,7 @@ def authorized_users_only(func):
     return wrapper
 
 def clean_youtube_url(url):
-    if not url or len(url) > 512:
+    if not url or len(url) > 2048:
         return None
     try:
         if not url.startswith(('http://', 'https://')):
